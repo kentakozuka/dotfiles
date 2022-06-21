977b112e59a25c1d663a6f08592a1f8f89347c01
Kenta Kozuka <kentakozuka@users.noreply.github.com>
2022-06-22 07:51:00 +0900

    Fix install.sh

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ ln -sf ${PWD}/zsh/.zshrc ${HOME}/.zshrc
 
 echo "I'm on $(uname)"
 if [ "$(uname)" = "Darwin" ] ; then
-    ln -sf ${PWD}/ssh/config ${HOME}/.ssh/config
-	ln -sf ${PWD}/karabiner/karabiner.json ${HOME}/.config/karabiner/karabiner.json
+    mkdir -p ${HOME}/.ssh &&ln -sf ${PWD}/ssh/config ${HOME}/.ssh/config
+	mkdir -p ${HOME}/.config/karabiner && ln -sf ${PWD}/karabiner/karabiner.json ${HOME}/.config/karabiner/karabiner.json
     ln -sf ${PWD}/tmux/.tmux.conf ${HOME}/.tmux.conf
     ln -sf ${PWD}/tmux/status.sh ${HOME}/.tmux/status.sh
 fi
