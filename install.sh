48f7b5d93717370e70d9565caeba8621d6374aca
Kenta Kozuka <kentakozuka@users.noreply.github.com>
2022-06-23 08:08:39 +0900

    wip

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ fi
 
 ln -sf ${PWD}/git/.gitconfig ${HOME}/.gitconfig
 ln -sf ${PWD}/zsh/.zshrc ${HOME}/.zshrc
+mkdir -p ${HOME}/.ssh &&ln -sf ${PWD}/ssh/config ${HOME}/.ssh/config
 
 echo "I'm on $(uname)"
 if [ "$(uname)" = "Darwin" ] ; then
-    mkdir -p ${HOME}/.ssh &&ln -sf ${PWD}/ssh/config ${HOME}/.ssh/config
 	mkdir -p ${HOME}/.config/karabiner && ln -sf ${PWD}/karabiner/karabiner.json ${HOME}/.config/karabiner/karabiner.json
     ln -sf ${PWD}/tmux/.tmux.conf ${HOME}/.tmux.conf
     mkdir -p ${HOME}/.tmux && ln -sf ${PWD}/tmux/status.sh ${HOME}/.tmux/status.sh
