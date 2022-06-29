c0cb2a2987bfec769d7430983a0fbe28b6b9fb32
Kenta Kozuka <kentakozuka@users.noreply.github.com>
2022-06-29 21:29:03 +0900

    fix install.sh

--- vscode/install-extensions.sh
+++ vscode/install-extensions.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+if ! command -v code &> /dev/null
+then
+    echo "code could not be found"
+    exit
+fi
+
 extension_file="$(pwd)/extensions"
 
 cat $extension_file | while read line
