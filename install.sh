40ad64a3100562cf0d41c17b25d19d453854499c
Kenta Kozuka <kentakozuka@users.noreply.github.com>
2022-07-17 08:45:31 +0900

    Add vscode keybindings.json

--- install.sh
+++ install.sh
@@ -12,5 +12,6 @@ echo "I'm on $(uname)"
 if [ "$(uname)" = "Darwin" ] ; then
 	mkdir -p ${HOME}/.config/karabiner && ln -sf ${PWD}/karabiner/karabiner.json ${HOME}/.config/karabiner/karabiner.json
     mkdir -p ${HOME}/Library/Application\ Support/Code/User && ln -sf ${PWD}/vscode/settings.json ${HOME}/Library/Application\ Support/Code/User/settings.json
+    mkdir -p ${HOME}/Library/Application\ Support/Code/User && ln -sf ${PWD}/vscode/keybindings.json ${HOME}/Library/Application\ Support/Code/User/keybindings.json
     (cd vscode && source install-extensions.sh)
 fi
