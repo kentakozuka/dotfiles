fc3db1c4bdc3e426dda87c2d81e6d469cbe298a2
Kenta Kozuka <kenta.kozuka@gmail.com>
2023-11-06 22:34:12 +0900

    Remove install.sh
    Signed-off-by: Kenta Kozuka <kenta.kozuka@gmail.com>

--- install.sh
+++ install.sh
@@ -3,16 +3,16 @@
 set -e
 
 # Install asdf
-if ! command -v asdf &> /dev/null
-then
-		git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.12.0
-fi
+# if ! command -v asdf &> /dev/null
+# then
+# 		git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.12.0
+# fi
 
-ln -sf ${PWD}/git/.gitconfig ${HOME}/.gitconfig
-ln -sf ${PWD}/zsh/.zshrc ${HOME}/.zshrc
-mkdir -p ${HOME}/.ssh &&ln -sf ${PWD}/ssh/config ${HOME}/.ssh/config
+# ln -sf ${PWD}/git/.gitconfig ${HOME}/.gitconfig
+# ln -sf ${PWD}/zsh/.zshrc ${HOME}/.zshrc
+# mkdir -p ${HOME}/.ssh &&ln -sf ${PWD}/ssh/config ${HOME}/.ssh/config
 
-echo "I'm on $(uname)"
-if [ "$(uname)" = "Darwin" ] ; then
-	mkdir -p ${HOME}/.config/karabiner && ln -sf ${PWD}/karabiner/karabiner.json ${HOME}/.config/karabiner/karabiner.json
-fi
+# echo "I'm on $(uname)"
+# if [ "$(uname)" = "Darwin" ] ; then
+# 	mkdir -p ${HOME}/.config/karabiner && ln -sf ${PWD}/karabiner/karabiner.json ${HOME}/.config/karabiner/karabiner.json
+# fi
