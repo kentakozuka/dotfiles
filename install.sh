70cc4560db24452b144628c679c955ca3d8648b0
Kenta Kozuka <kenta.kozuka@gmail.com>
2024-02-09 22:16:22 +0900

    Add default keybinding for mac
    Signed-off-by: Kenta Kozuka <kenta.kozuka@gmail.com>

--- install.sh
+++ install.sh
@@ -15,4 +15,7 @@ mkdir -p ${HOME}/.ssh &&ln -sf ${PWD}/ssh/config ${HOME}/.ssh/config
 echo "I'm on $(uname)"
 if [ "$(uname)" = "Darwin" ] ; then
 	mkdir -p ${HOME}/.config/karabiner && ln -sf ${PWD}/karabiner/karabiner.json ${HOME}/.config/karabiner/karabiner.json
+
+	mkdir -p ${HOME}/Library/KeyBindings
+	cp keybinding/DefaultKeybinding.dict ${HOME}/Library/KeyBindings/
 fi
