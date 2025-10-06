48c959b2b7fe12e03658bf174f7b2fdd5bc6b131
Kenta Kozuka <kenta.kozuka@gmail.com>
2025-10-07 00:17:32 +0900

    setup chezmoi in install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ if [ "${CODESPACES}" = "true" ] ; then
 fi
 
 SCRIPTPATH="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)"
+ln -sf ${SCRIPTPATH} ${HOME}/.local/share/chezmoi
 ln -sf ${SCRIPTPATH}/git/.gitconfig ${HOME}/.gitconfig
 ln -sf ${SCRIPTPATH}/zsh/.zshrc ${HOME}/.zshrc
 mkdir -p ${HOME}/.ssh && ln -sf ${SCRIPTPATH}/ssh/config ${HOME}/.ssh/config
@@ -16,3 +17,5 @@ if [ "$(uname)" = "Darwin" ] ; then
 	mkdir -p ${HOME}/.config/karabiner && ln -sf ${SCRIPTPATH}/karabiner/karabiner.json ${HOME}/.config/karabiner/karabiner.json
 	mkdir -p ${HOME}/Library/KeyBindings && ln -sf ${SCRIPTPATH}/keybinding/DefaultKeybinding.dict ${HOME}/Library/KeyBindings/
 fi
+
+ln -sf ~/ghq/github.com/kentakozuka/dotfiles ${HOME}/.local/share/chezmoi
