6b1074ea3959064bdcf0630a4f8dd666c77a1faf
Kenta Kozuka <kenta.kozuka@gmail.com>
2025-10-07 09:54:41 +0900

    update hammerspoon to set up keybind

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -134,6 +134,62 @@ hs.hotkey.bind({"ctrl", "alt"}, "C", function()
   win:setFrame(f)
 end)
 
+local function keyCode(key, modifiers)
+  modifiers = modifiers or {}
+  return function()
+    hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), true):post()
+    hs.timer.usleep(1000)
+    hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), false):post()
+  end
+end
+
+local function keyCodeSet(keys)
+  return function()
+    for i, keyEvent in ipairs(keys) do
+      keyEvent()
+    end
+  end
+end
+
+local function remapKey(modifiers, key, keyCode)
+  hs.hotkey.bind(modifiers, key, keyCode, nil, keyCode)
+end
+
+remapKey({ 'ctrl' }, '[',  keyCodeSet({
+    keyCode('escape'),
+    keyCode(';', {'ctrl', 'shift'})
+}))
+
+-- Switc kana/alphanumeric.
+-- https://zenn.dev/obregonia1/articles/419ae303355f54
+-- Why simply hotkey.bind does not work?
+-- https://mac-ra.com/hammerspoon-command-eikana02/
+local simpleCmd = false
+local map = hs.keycodes.map
+local function eikanaEvent(event)
+  local c = event:getKeyCode()
+  local f = event:getFlags()
+  if event:getType() == hs.eventtap.event.types.keyDown then
+    if f['cmd'] then
+      simpleCmd = true
+    end
+  elseif event:getType() == hs.eventtap.event.types.flagsChanged then
+    if not f['cmd'] then
+      if simpleCmd == false then
+        if c == map['cmd'] then
+          hs.keycodes.setMethod('Alphanumeric (Google)')
+        elseif c == map['rightcmd'] then
+          hs.keycodes.setMethod('Hiragana (Google)')
+        end
+      end
+      simpleCmd = false
+    end
+  end
+end
+
+eikana = hs.eventtap.new({hs.eventtap.event.types.keyDown, hs.eventtap.event.types.flagsChanged}, eikanaEvent)
+eikana:start()
+
 -- Reload config
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
   hs.reload()
