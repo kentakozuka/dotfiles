16396989003c5bb696fd41c5c1d2fa85bddd5ee2
Kenta Kozuka <kenta.kozuka@gmail.com>
2025-10-23 11:05:18 +0900

    remove chezmoi

--- install.sh
+++ install.sh
@@ -5,17 +5,18 @@ if [ "${CODESPACES}" = "true" ] ; then
 fi
 
 SCRIPTPATH="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)"
-ln -sf ${SCRIPTPATH} ${HOME}/.local/share/chezmoi
-ln -sf ${SCRIPTPATH}/git/.gitconfig ${HOME}/.gitconfig
-ln -sf ${SCRIPTPATH}/zsh/.zshrc ${HOME}/.zshrc
-mkdir -p ${HOME}/.ssh && ln -sf ${SCRIPTPATH}/ssh/config ${HOME}/.ssh/config
-source ${SCRIPTPATH}/vscode/install-extensions.sh
 
-echo "I'm on $(uname)"
+ln -sf ${SCRIPTPATH}/git/.gitconfig ${HOME}/.gitconfig
+ln -sf ${SCRIPTPATH}/git/.gitignore ${HOME}/.gitignore
+ln -sf ${SCRIPTPATH}/zsh/.zshrc ${HOME}/.zshrc
+ln -sf ${SCRIPTPATH}/asdf/.asdfrc ${HOME}/.asdfrc
+ln -sf ${SCRIPTPATH}/vim/.vimrc ${HOME}/.vimrc
+mkdir -p ${HOME}/.ssh && ln -sf ${SCRIPTPATH}/ssh/config ${HOME}/.ssh/config
+mkdir -p ${HOME}/.aws && ln -sf ${SCRIPTPATH}/aws/amazonq ${HOME}/.aws/amazonq
+mkdir -p ${HOME}/.hammerspoon && ln -sf ${SCRIPTPATH}/hammerspoon/init.lua ${HOME}/.hammerspoon/init.lua
+
 if [ "$(uname)" = "Darwin" ] ; then
-	SCRIPTPATH="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)"
 	mkdir -p ${HOME}/.config/karabiner && ln -sf ${SCRIPTPATH}/karabiner/karabiner.json ${HOME}/.config/karabiner/karabiner.json
-	mkdir -p ${HOME}/Library/KeyBindings && ln -sf ${SCRIPTPATH}/keybinding/DefaultKeybinding.dict ${HOME}/Library/KeyBindings/
 fi
 
-ln -sf ~/ghq/github.com/kentakozuka/dotfiles ${HOME}/.local/share/chezmoi
+source ${SCRIPTPATH}/vscode/install-extensions.sh
