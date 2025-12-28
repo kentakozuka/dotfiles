0a0f56437f421b0f2e04022f1ef4643b8b09ed9b
Kenta Kozuka <kenta.kozuka@gmail.com>
2025-12-29 00:30:14 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -11,12 +11,12 @@ ln -sf ${SCRIPTPATH}/git/.gitignore ${HOME}/.gitignore
 ln -sf ${SCRIPTPATH}/zsh/.zshrc ${HOME}/.zshrc
 ln -sf ${SCRIPTPATH}/asdf/.asdfrc ${HOME}/.asdfrc
 ln -sf ${SCRIPTPATH}/vim/.vimrc ${HOME}/.vimrc
+ln -sf ${SCRIPTPATH}/vim/init.lua ${HOME}/.config/nvim/init.lua
 mkdir -p ${HOME}/.ssh && ln -sf ${SCRIPTPATH}/ssh/config ${HOME}/.ssh/config
-mkdir -p ${HOME}/.aws && ln -sf ${SCRIPTPATH}/aws/amazonq ${HOME}/.aws/amazonq
 mkdir -p ${HOME}/.hammerspoon && ln -sf ${SCRIPTPATH}/hammerspoon/init.lua ${HOME}/.hammerspoon/init.lua
 
 if [ "$(uname)" = "Darwin" ] ; then
 	mkdir -p ${HOME}/.config/karabiner && ln -sf ${SCRIPTPATH}/karabiner/karabiner.json ${HOME}/.config/karabiner/karabiner.json
 fi
 
-source ${SCRIPTPATH}/vscode/install-extensions.sh
+#source ${SCRIPTPATH}/vscode/install-extensions.sh
